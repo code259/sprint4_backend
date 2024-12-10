2a838abe6fec83eea97feb1f6230778b2959c9cf
code259 <nikhilmaturi259@gmail.comm>
2024-12-10 13:58:37 -0800

    Added personal static API.
    Co-authored-by: Jowan3 <Jowan3@users.noreply.github.com>
    Co-authored-by: Vasanth-Rajasekaran <Vasanth-Rajasekaran@users.noreply.github.com>
    Co-authored-by: Aarush Gowda <NeonMist27@users.noreply.github.com>
    Co-authored-by: Mihirt5 <Mihirt5@users.noreply.github.com>
    Co-authored-by: nikhilsna
    <nikhilsna@users.noreply.github.com>

--- api/personalInfo.py
+++ api/personalInfo.py
@@ -6,16 +6,39 @@ student_api = Blueprint('student_api', __name__, url_prefix='/api')
 api = Api(student_api)
 
 class StudentAPI:
-    class _Nikhil(Resource):
+    class _NikhilM(Resource):
         def get(self):
             data = {'DOB': '01-20-2009', 'Name': 'Nikhil Maturi', 'Favorite Color': 'Black'}
             return jsonify(data)
-
     class _Mihir(Resource):
         def get(self):
-            data = {'DOB': '07-26-2009', 'Name': 'Mihir Thaha', 'Favorite Color': 'Green'}
+            data = {'DOB': '04-09-2009', 'Name': 'Mihir Thaha', 'Favorite Color': 'Purple'}
+            return jsonify(data)
+
+    class _Aarush(Resource):
+        def get(self):
+            data = {'DOB': '04-09-2009', 'Name': 'Aarush Gowda', 'Favorite Color': 'Grey'}
+            return jsonify(data)
+    class _Vasanth(Resource):
+        def get(self):
+            data = {'DOB': '06-27-2009', 'Name': 'Vasath Rajasekaran', 'Favorite Color': 'Black'}
+            return jsonify(data)
+
+    class _NikhilN(Resource):
+        def get(self):
+            data = {'DOB': '05-16-2009', 'Name': 'Nikhil Narayan', 'Favorite Color': 'Blue'}
+            return jsonify(data)
+        
+    class _Jowan(Resource):
+        def get(self):
+            # Use the helper method to get Jeff's details
+            data = {'DOB': '07-02-2008', 'Name': 'Jowan Elzein', 'Favorite Color': 'Purple'}
             return jsonify(data)
 
     # building RESTapi endpoint
-    api.add_resource(_Nikhil, '/student/nikhil')
+    api.add_resource(_NikhilM, '/student/nikhilm')
     api.add_resource(_Mihir, '/student/mihir')
+    api.add_resource(_Aarush, '/student/aarush')
+    api.add_resource(_NikhilN, '/student/nikhiln')
+    api.add_resource(_Jowan, '/student/jowan')
+    api.add_resource(_Vasanth, '/student/vasanth')
