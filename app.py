f84f87c528a56f22fcfb24b2165262756d8d9bde
code259 <nikhilmaturi259@gmail.comm>
2024-12-10 13:46:36 -0800

    Finished base api framework

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# initialize a flask application (app)
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
+
+# ... your existing Flask
+
+# add an api endpoint to flask app
+@app.route('/api/john')
+def get_data():
+    # start a list, to be used like a information database
+    InfoDb = []
+
+    # add a row to list, an Info record
+    InfoDb.append({
+        "FirstName": "John",
+        "LastName": "Mortensen",
+        "DOB": "October 21",
+        "Residence": "San Diego",
+        "Email": "jmortensen@powayusd.com",
+        "Owns_Cars": ["2015-Fusion", "2011-Ranger", "2003-Excursion", "1997-F350", "1969-Cadillac"]
+    })
+
+    # add a row to list, an Info record
+    InfoDb.append({
+        "FirstName": "Shane",
+        "LastName": "Lopez",
+        "DOB": "February 27",
+        "Residence": "San Diego",
+        "Email": "slopez@powayusd.com",
+        "Owns_Cars": ["2021-Insight"]
+    })
+
+    return jsonify(InfoDb)
+
+
+if __name__ == '__main__':
+    # starts flask server on default port, http://127.0.0.1:5001
+    app.run(port=5001)
