4dee14ae1859c11f83790043a33abffe2e4d4090
code259 <nikhilmaturi259@gmail.comm>
2024-12-11 08:26:58 -0800

    Created dynamic retrieval

--- api/personalInfo.py
+++ api/personalInfo.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
-from flask_restful import Api, Resource # used for REST API building
+from flask_restful import Api, Resource
+from model.personalInfo import personalInfo
 
 student_api = Blueprint('student_api', __name__, url_prefix='/api')
 
@@ -34,16 +35,25 @@ class StudentAPI:
             data = {'DOB': '07-02-2008', 'Name': 'Jowan Elzein', 'Favorite Color': 'Purple'}
             return jsonify(data)
 
-    class _BULK_INFO(Resource):
+    class _BULK_STATIC(Resource):
         def get(self):
             data = {'Nikhilm': {'DOB': '01-20-2009', 'Name': 'Nikhil Maturi', 'Favorite Color': 'Black'}, 'Mihir': {'DOB': '04-09-2009', 'Name': 'Mihir Thaha', 'Favorite Color': 'Purple'}, 'Nikhiln': {'DOB': '05-16-2009', 'Name': 'Nikhil Narayan', 'Favorite Color': 'Blue'}, 'Aarush': {'DOB': '04-09-2009', 'Name': 'Aarush Gowda', 'Favorite Color': 'Grey'}, 'Vasanth': {'DOB': '06-27-2009', 'Name': 'Vasanth Rajasekaran', 'Favorite Color': 'Black'}, 'Jowan': {'DOB': '07-02-2008', 'Name': 'Jowan Elzein', 'Favorite Color': 'Purple'}}
             return jsonify(data)
 
-    # building RESTapi endpoint
+    class _BULK_DYNAMIC(Resource):
+        def get(self):
+            infos = personalInfo.query.all()
+            json_ready = []
+            for info in infos:
+                info_data = info.read()
+                json_ready.append(info_data)
+            return jsonify(json_ready)
+
     api.add_resource(_NikhilM, '/student/nikhilm')
     api.add_resource(_Mihir, '/student/mihir')
     api.add_resource(_Aarush, '/student/aarush')
     api.add_resource(_NikhilN, '/student/nikhiln')
     api.add_resource(_Jowan, '/student/jowan')
     api.add_resource(_Vasanth, '/student/vasanth')
-    api.add_resource(_BULK_INFO, '/student/bulk')
+    api.add_resource(_BULK_STATIC, '/student/bulk_static')
+    api.add_resource(_BULK_DYNAMIC, '/student/bulk_dynamic')
